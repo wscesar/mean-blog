1fdf46df1d54450d3aec8ba81a8bc1ffcaa09858
William Siqueira <wscesar@gmail.com>
2019-09-08 11:13:23 -0300

    added image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,17 +5,19 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
 
+
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-    form: FormGroup;
     enteredTitle = '';
     enteredContent = '';
     post: Post;
     isLoading = false;
+    form: FormGroup;
+    imagePreview: string;
     private mode = 'create';
     private postId: string;
 
@@ -26,59 +28,93 @@ export class PostCreateComponent implements OnInit {
 
     ngOnInit() {
         this.form = new FormGroup({
-            title: new FormControl(
-                null,
-                { validators: [Validators.required, Validators.minLength(3)] }
-            ),
-            content: new FormControl(
-                null,
-                { validators: [Validators.required] }
-            ),
-            file: new FormControl(null, Validators.required);
+
+            title: new FormControl(null, {
+                validators: [Validators.required, Validators.minLength(3)]
+            }),
+
+            content: new FormControl(null, {
+                validators: [Validators.required]
+            }),
+
+            image: new FormControl(null, {
+                validators: [Validators.required]
+            })
+
         });
+
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
+
             if (paramMap.has('postId')) {
+
                 this.mode = 'edit';
+
                 this.postId = paramMap.get('postId');
+
                 this.isLoading = true;
+
                 this.postsService.getPost(this.postId).subscribe(postData => {
                     this.isLoading = false;
                     this.post = { id: postData._id, title: postData.title, content: postData.content };
                 });
+
                 this.form.setValue({
                     title: this.post.title,
                     content: this.post.content
                 });
+
             } else {
                 this.mode = 'create';
                 this.postId = null;
             }
+
         });
     }
 
     onImagePicked(event: Event) {
+
         const file = (event.target as HTMLInputElement).files[0];
+
         this.form.patchValue({ image: file });
+
         this.form.get('image').updateValueAndValidity();
+
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            this.imagePreview = reader.result as string;
+        };
+
+        reader.readAsDataURL(file);
+
     }
 
     onSavePost(form: NgForm) {
+
         if (form.invalid) {
             return;
         }
+
         this.isLoading = true;
+
         if (this.mode === 'create') {
+
             this.postsService.addPost(
                 this.form.value.title,
                 this.form.value.content
             );
+
         } else {
+
             this.postsService.updatePost(
                 this.postId,
                 this.form.value.title,
                 this.form.value.content
             );
+
         }
+
         form.resetForm();
     }
+
 }
